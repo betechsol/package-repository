0da1aee96271b0c57e670012611892db26ea8548
betechinc <betechincorporated@gmail.com>
2025-05-23 19:50:35 -0400

    Update ubuntu-maven.sh

--- Maven-installation/ubuntu-maven.sh
+++ Maven-installation/ubuntu-maven.sh
@@ -3,7 +3,6 @@
 sudo apt update
 wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz -P /tmp
 sudo tar -xf /tmp/openjdk-13.0.1_linux-x64_bin.tar.gz -C /opt
-sudo mv jdk-13.0.1 /opt/
 wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz -P /tmp
 sudo tar -xf /tmp/apache-maven-3.9.9-bin.tar.gz -C /opt
 sudo ln -s /opt/apache-maven-3.9.9 /opt/maven
@@ -12,6 +11,7 @@ export JAVA_HOME=/opt/jdk-13.0.1
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 export PATH=${M2_HOME}/bin:${PATH}
+export PATH
 EOF
 sudo chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
