c90f7376a4437fc886004a95e9034dd25cb40b6d
betechinc <betechincorporated@gmail.com>
2025-05-31 05:24:16 -0400

    Update ubuntu-tomcat.sh

--- Tomcat-installation/ubuntu-tomcat.sh
+++ Tomcat-installation/ubuntu-tomcat.sh
@@ -10,3 +10,11 @@ sudo chgrp -R tomcat /opt/tomcat
 sudo chmod -R g+r conf
 sudo chmod g+x conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
+cd /opt/tomcat/bin
+# Used to run tomcat
+# sudo /opt/tomcat/bin/startup.sh run
+# create a soft link to start and stop tomcat
+# This will enable us to manage tomcat as a service
+sudo ln -s /opt/tomcat/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat/bin/shutdown.sh /usr/bin/stoptomcat
+starttomcat
