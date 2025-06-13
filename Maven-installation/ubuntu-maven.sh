a1f1b189b03250859abca9bd04620014d0183d50
betechinc <betechincorporated@gmail.com>
2025-06-13 12:33:44 -0400

    Update ubuntu-maven.sh

--- Maven-installation/ubuntu-maven.sh
+++ Maven-installation/ubuntu-maven.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 ##Installation Of Maven Server
 sudo apt update
-wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz -P /tmp
-sudo tar -xf /tmp/openjdk-13.0.1_linux-x64_bin.tar.gz -C /opt
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz -P /tmp
-sudo tar -xf /tmp/apache-maven-3.9.9-bin.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-3.9.9 /opt/maven
+wget https://download.oracle.com/java/17/archive/jdk-17.0.1_linux-x64_bin.tar.gz -P /tmp
+sudo tar -xf /tmp/jdk-17.0.1_linux-x64_bin.tar.gz -C /opt
+wget https://dlcdn.apache.org/maven/maven-3/3.9.10/binaries/apache-maven-3.9.10-bin.tar.gz -P /tmp
+sudo tar -xf /tmp/apache-maven-3.9.10-bin.tar.gz -C /opt
+sudo ln -s /opt/apache-maven-3.9.10 /opt/maven
 sudo cat > /etc/profile.d/maven.sh<< EOF
-export JAVA_HOME=/opt/jdk-13.0.1
+export JAVA_HOME=/opt/jdk-17.0.1
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 export PATH=${M2_HOME}/bin:${PATH}
